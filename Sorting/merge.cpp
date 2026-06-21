9ad49301bf1a03dde0b6cbbfcaab1150e688f239
Vaidik Khandelwal <khandelwalvaidik122333@gmail.com>
2026-06-21 23:47:03 +0530

    add merge sort implementation in c++

---
+++ Sorting/merge.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void merge (vector<int> &arr,int low,int mid,int high){
+    vector<int> temparr;
+    int left = low;
+    int right = mid + 1;
+
+
+    while(left<=mid && right <=high){
+        if(arr[left]<=arr[right]){
+            temparr.push_back(arr[left]);
+            left++;
+        }else {
+            temparr.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temparr.push_back(arr[left]);
+        left++;       
+    }
+    while(right<=high){
+        temparr.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++)
+    {
+        arr[i] =temparr[i-low];
+    }
+
+
+}
+void mergesort(vector<int>&arr,int low, int high) {
+    if(low>=high) return;
+    else {
+        int mid = (low + high)/2;
+
+        mergesort(arr,low,mid);
+
+        mergesort(arr,mid+1,high);
+
+        merge(arr,low,mid,high);
+    }
+}
+
+
+
+int main() {
+
+    int n;
+    cout<<"size of array:"<<endl;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    mergesort(arr,0,n-1);
+
+    for(int i =0;i<n;i++){
+        cout<< arr[i]<<"\t";
+    }
+
+    return 0;
+}
