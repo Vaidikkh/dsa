11be1d47321c955ffeaf3f559272305de9347fc3
Vaidik Khandelwal <khandelwalvaidik122333@gmail.com>
2026-06-25 23:55:06 +0530

    checking the array is sorted or nt

--- Sorting/check_sort.cpp
+++ Sorting/check_sort.cpp
@@ -2,6 +2,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int check_sorted(const vector<int>&arr){
+    for(int i=1;i<(int)arr.size();i++){
+        if(arr[i]>=arr[i-1]){
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(){
+    int n;
+    cout<<"Size of array"<<endl;
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter the array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    check_sorted(arr);
     
+    return 0;
+
 }
